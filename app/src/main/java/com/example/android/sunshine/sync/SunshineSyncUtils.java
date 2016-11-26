0bc987704e827155b3df2ff2945d6e1f929d580b
Asser Samak <asser@asamak.local>
2016-11-26 13:30:26 -0600

    S10.01-Exercise-SmarterSyncing

---
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -0,0 +1,12 @@
+// TODO (9) Create a class called SunshineSyncUtils
+
+//      TODO (12) Declare a private static boolean field called sInitialized
+
+//  TODO (13) Create a synchronized public static void method called initialize
+
+//      TODO (14) Only execute this method body if sInitialized is false
+
+//      TODO (15) If sInitialized is false, set it to true and call startImmediateSync
+
+//  TODO (10) Create a public static void method called startImmediateSync
+//      TODO (11) Within that method, start the SunshineSyncIntentService
